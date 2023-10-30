9907313636b55e48cfcab8f3fc787be8b75a8124
Fernando Abolafio <fernandohsabolafio@gmail.com>
2023-10-30 15:31:12 +0100

    add benchmark tests

---
+++ test/helpers/create-large-test-cube.js
@@ -0,0 +1,45 @@
+const { Cube, GenericDimension } = require('../../src');
+
+module.exports = (
+    numOfDimensions,
+    dimensionSize,
+    numOfMeasures,
+    defaultMeasureValue = 1,
+    sparseDataRate
+) => {
+    const dimensions = [];
+
+    for (let i = 0; i < numOfDimensions; i++) {
+        const dimension = new GenericDimension(
+            `dimension${i}`,
+            'root',
+            Array.from({ length: dimensionSize }, (_, j) => `dimension${i}-item${j}`)
+        );
+        dimensions.push(dimension);
+    }
+
+    const cube = new Cube(dimensions);
+
+    for (let i = 0; i < numOfMeasures; i++) {
+        cube.createStoredMeasure(`measure${i}`, {}, 'float32', defaultMeasureValue);
+    }
+
+    const sizeToFillDataRandomly = sparseDataRate * cube.storeSize;
+
+    const usedIndexes = {};
+    for (let i = 0; i < sizeToFillDataRandomly; i++) {
+        let currIndex = 0;
+
+        while (usedIndexes[currIndex]) {
+            currIndex = Math.floor(Math.random() * cube.storeSize);
+        }
+
+        usedIndexes[currIndex] = true;
+
+        cube.storedMeasureIds.forEach(measureId => {
+            cube.storedMeasures[measureId].setValue(currIndex, 1);
+        });
+    }
+
+    return cube;
+};
